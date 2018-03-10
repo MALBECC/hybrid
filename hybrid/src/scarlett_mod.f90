ab21002ba40fcd671bc4789b2fb95b4389a9dfdc
Nick <nico2711@gmail.com>
2018-03-09 21:54:43 -0300

    starting to move a few variables to a scarlett general module and inicialization to init_hybrid

--- hybrid/src/scarlett_mod.f90
+++ hybrid/src/scarlett_mod.f90
@@ -1,5 +1,19 @@
 	module scarlett
-!starting to move all variables to this module 03/2018
+!general module for hybrid
+!In future i will move most variables to this module 03/2018
 	implicit none
-	double precision, allocatable, dimension(:,:,:) :: aclas_BAND_old
+
+! Solvent (MM) General variables
+	integer :: natot !total number of atoms
+
+!NEB variables
+	integer :: replicas !number of replicas in band methods
+	double precision, allocatable, dimension(:,:,:) :: aclas_BAND_old !aceleration of previus step
+	double precision, dimension(:,:,:), allocatable, save:: rclas_BAND!Position of all atoms in BAND method
+	double precision, dimension(:,:,:), allocatable, save:: vclas_BAND!velocities of all atoms in BAND method
+	double precision, dimension(:,:,:), allocatable, save:: fclas_BAND!Force all atoms in BAND method
+	double precision, dimension(:), allocatable :: Energy_band !Energy of each image
+
+
+
 	end module scarlett
