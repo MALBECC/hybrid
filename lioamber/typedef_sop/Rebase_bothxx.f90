d3dfc0aa1444017aab2b0705c2be2ba57161db72
Nick <nico2711@gmail.com>
2018-09-19 12:52:53 -0300

    Solving Stack-overflow in big systems. Part 1: matmul3

--- lioamber/typedef_sop/Rebase_bothxx.f90
+++ lioamber/typedef_sop/Rebase_bothxx.f90
@@ -6,7 +6,7 @@ subroutine Rebase_fockao( this, fock )
    real*8    , intent(inout) :: fock( this%Nbasis, this%Nbasis )
 
 !  From AO to ON
-   fock = matmul3( this%Xtrp, fock, this%Xmat )
+   call matmul3( this%Xtrp, fock, this%Xmat, fock )
 
 end subroutine Rebase_fockao
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
@@ -17,7 +17,7 @@ subroutine Rebase_fockon( this, fock )
    real*8    , intent(inout) :: fock( this%Nbasis, this%Nbasis )
 
 !  From ON to AO
-   fock = matmul3( this%Ymat, fock, this%Ytrp )
+   call matmul3( this%Ymat, fock, this%Ytrp, fock )
 
 end subroutine Rebase_fockon
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
@@ -28,7 +28,7 @@ subroutine Rebase_densao( this, dens )
    complex*16, intent(inout) :: dens( this%Nbasis, this%Nbasis )
 
 !  From AO to ON
-   dens = matmul3( this%Ytrp, dens, this%Ymat )
+   call matmul3( this%Ytrp, dens, this%Ymat, dens )
 
 end subroutine Rebase_densao
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
@@ -39,7 +39,7 @@ subroutine Rebase_denson( this, dens )
    complex*16, intent(inout) :: dens( this%Nbasis, this%Nbasis )
 
 !  From ON to AO
-   dens = matmul3( this%Xmat, dens, this%Xtrp )
+   call matmul3( this%Xmat, dens, this%Xtrp, dens )
 
 end subroutine Rebase_denson
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
