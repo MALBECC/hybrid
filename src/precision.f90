2188c5bb032831574ae074cfb02cacef062881a2
Nick <nico2711@gmail.com>
2019-07-29 14:21:50 -0300

    included LIOHOME and LIOlibs

--- src/precision.f90
+++ src/precision.f90
@@ -1,6 +1,16 @@
-      module precision
-      integer, parameter :: sp = selected_real_kind(6,30)
-      integer, parameter :: dp = selected_real_kind(14,100)
-      integer, parameter :: qp = selected_real_kind(33, 4931)
-      end module precision
+	module precision
+!old program presicions
+	integer, parameter :: sp = selected_real_kind(6,30)
+	integer, parameter :: dp = selected_real_kind(14,100)
+	integer, parameter :: qp = selected_real_kind(33, 4931)
+
+!this will be the program presicion in future modifications
+#ifdef SPRESS
+	integer, parameter :: xp = selected_real_kind(6, 37)
+#elif DPRESS
+	integer, parameter :: xp = selected_real_kind(15, 307)
+#elif QPRESS
+	integer, parameter :: xp = selected_real_kind(33, 4931)
+#endif
+	end module precision
 
